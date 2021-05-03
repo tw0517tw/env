19c82b323c4d4471f351c1a699f7b3d6a57a54f2
tw0517tw <tw0517tw@gmail.com>
2021-05-03 19:55:54 +0800

    update config

--- configs/PowerShell/Microsoft.PowerShell_profile.ps1
+++ configs/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,23 @@
 # $Host.UI.RawUI.BackgroundColor = "black"
 
 Set-PSReadlineOption -BellStyle None
+Set-PSReadLineOption -Colors @{
+    Operator           = 'Yellow'
+    Parameter          = 'Yellow'
+}
 
 Import-Module posh-git
 
 Set-alias k kubectl
+Set-alias kc kubectx
+Set-alias kn kubens
+Set-alias h helm
+Set-alias h2 "helm2.16.10"
 # Set-alias python3 "C:\python35\python.exe"
 # Set-alias py3 python3
 # Set-alias pip3 "C:\Python35\Scripts\pip.exe"
 Set-alias which Get-Command
+Set-alias grep Select-String
 
 function grpo {
     git remote prune origin
@@ -34,5 +43,3 @@ function Get-Font {
 function yt {
     Set-Location C:\Users\tw0517tw\yoctol\$args
 }
-
-Clear-Host
